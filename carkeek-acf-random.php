6aa442ab2275c6a702559ff5941ebe5031651b57
Patty O'Hara <pattyohara@Pattys-Mac-Studio.local>
2024-09-20 11:17:34 -0700

    version bump

--- carkeek-acf-random.php
+++ carkeek-acf-random.php
@@ -8,7 +8,7 @@
  * Primary Branch: main
  * Description: Custom block to display a random image from a given set of images.
  * Author: Patty O'Hara
- * Version: 0.0.03
+ * Version: 0.0.04
  * Author URI https://carkeekstudios.com/
  * Text Domain: carkeek-blocks
  */
